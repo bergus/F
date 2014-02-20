7031b0d39c09868f22571bb33c317ffb1e4867c8
Bergi <a.d.bergi@web.de>
2014-02-20 03:48:09 +0100

    docs for F()

--- F.js
+++ F.js
@@ -3,6 +3,12 @@
     var opcache = F.opcache = {};
     
     function F(op) {
+        // create functions for operators | no eval magic!
+        // with a Scala-like lamda expression syntax (but no access to local variables)
+        // Examples: F("+") == F("_+_"), F("_?_:_").toString(), F("+")(1,2) == 3, F("-_")(5) == -5, F("_+1")(2) == 3
+        // DO NOT use too extensively, functions are globally cached
+        // DO NOT use for partial application, like var x=1; F("_+"+x)(2) == 3
+        // @TODO: Currying
         if (op in opcache)
             return opcache[op];
         var i = 0,
