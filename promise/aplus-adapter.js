c69ffdf4b8e2d2ba10e5e807e941e65a987283f5
Bergi <a.d.bergi@web.de>
2014-08-02 18:47:59 +0200

    [minor] accurate warning messages for adapter

--- promise/aplus-adapter.js
+++ promise/aplus-adapter.js
@@ -1,6 +1,6 @@
 var Promise = require("./Promise.js");
 exports.resolved = function(v) {
-	if (v && (v instanceof Promise || {}.hasOwnProperty.call(v, "then"))) console.error("this will not make you lucky");
+	if (v instanceof Promise || v && {}.hasOwnProperty.call(v, "then")) console.warn("This would not make you lucky if it was Promise.of");
 	return Promise.resolve(v);
 };
 exports.rejected = Promise.reject;
@@ -8,10 +8,10 @@ exports.deferred = function() {
 	var d = {};
 	d.promise = new Promise(function(fulfill, reject) {
 		d.resolve = function(v) {
-			if (v instanceof Promise) console.error("Don't call deferred.resolve with a promise");
+			if (v instanceof Promise || v && {}.hasOwnProperty.call(v, "then")) console.warn("Don't call deferred.resolve() with a promise when it might do a fulfill()");
 			fulfill.async(v);
 		};
 		d.reject = reject.async;
-	}).chain(Promise.resolve);
+	}).chain(Promise.from); // make .fulfill() a .resolve()
 	return d;
 };
